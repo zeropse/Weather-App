dc36c86af60d40e53cb80ce68d3f3cbcf6722482
zeropse <129523087+zeropse@users.noreply.github.com>
2025-01-06 23:11:44 +0530

    fix style

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center mt-10">
       <h1 className="text-4xl font-bold">Weather App</h1>
     </div>
   );
