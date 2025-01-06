39c137cf18c702a9102605831b99f94616618b60
zeropse <129523087+zeropse@users.noreply.github.com>
2025-01-06 22:43:43 +0530

    init repo

---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Weather App</h1>
+    </div>
+  );
+};
+
+export default Header;
