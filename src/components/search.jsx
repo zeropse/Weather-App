6cec2f1cde24974f851693e78cc269ab5dac1002
zeropse <129523087+zeropse@users.noreply.github.com>
2025-01-07 23:05:06 +0530

    added search queries

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,4 +1,24 @@
+import { useState } from "react";
+
 const Search = () => {
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    console.log(search);
+    setSearch("");
+  };
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleSearch(e);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center mt-10">
       <div className="flex items-center justify-center">
@@ -18,9 +38,16 @@ const Search = () => {
         <input
           type="text"
           placeholder="Search"
-          className="p-2 rounded-md cursor-pointer"
+          className="p-2 rounded-md cursor-pointer w-full text-black  bg-white"
+          value={search}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+          onSubmit={handleSearch}
         />
-        <button className="bg-[#000000] text-white p-2 rounded-md cursor-pointer">
+        <button
+          className="bg-[#000000] text-white p-2 rounded-md cursor-pointer"
+          onClick={handleSearch}
+        >
           Search
         </button>
       </form>
